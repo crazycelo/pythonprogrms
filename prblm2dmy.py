5951d2311f1914aa22814c6cf85cf6b012db5bc4
crazycelo <139037965+crazycelo@users.noreply.github.com>
2024-01-20 10:45:30 +0530

    Add files via upload

---
+++ prblm2dmy.py
@@ -0,0 +1,7 @@
+date=int(input("enter the dateinput:"))
+month=int(input("enter the monthinput:"))
+year=int(input("enter the yeatinput:"))
+input=[year,month,date]
+print("originaldate:",input) 
+converteddate=input.reverse()
+print("converteddate:",input)

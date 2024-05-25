f44abac6935252c2b445fca06d54c9ac4449157a
crazycelo <celoambrose2116@gmail.com>
2024-05-25 13:59:18 +0530

    files

---
+++ amazon.py
@@ -0,0 +1,11 @@
+'''input="zonama"
+print(input.split())
+a=input("enter the string:")
+n=int(input("enter the number:"))'''
+def rote(a,n):
+    print(a[n:]+a[:n])
+rote("zonama",3)    
+
+
+
+
---
+++ example01.py
@@ -0,0 +1,7 @@
+a=input("enter a name:")
+b=input("enter a name:")
+c=input("enter a name:")
+d=input("enter a name:")
+e=input("enter a name:")
+formated_string=f" my name is {a} \n my sister name is {b} \n my friend name is {c} \n my mother name is {d} \n my father name is {e}"
+print(formated_string)

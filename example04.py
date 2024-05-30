daa941f130cb48e524c0c4bd9d8b272d96da77f3
crazycelo <celoambrose2116@gmail.com>
2024-05-30 12:49:56 +0530

    new program

---
+++ example04.py
@@ -0,0 +1,18 @@
+x=int(input("enter the number of books:"))
+y=int(input("enter the total price of books:"))
+z=x*y
+
+#conditions
+
+if x>=2 and x<=4:
+    dis=z*10//100
+    print(z-dis)
+elif x==5:
+    dis=z*20//100
+    print(z-dis)
+elif x>=5:
+    dis=z*50//100
+    print(z-dis)  
+else:
+    print(z)    
+

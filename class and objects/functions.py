4cce317fe17c651b586cfba1bf8ecd8283895f74
crazycelo <celoambrose2116@gmail.com>
2024-06-13 12:42:15 +0530

    add files

---
+++ class and objects/functions.py
@@ -0,0 +1,26 @@
+'''def factorial(x):
+    if x==1:
+        return 1
+    else:
+        return (x+factorial(x-1))
+    
+print("factorial :",factorial(10)) '''   #recursive function
+
+
+
+#lambda function can take any number of arguments in single expression.
+'''x= lambda a,b,c:a+b+c
+print(x(1,2,3)) '''
+
+
+'''def myfunc(n):
+    return lambda a:a*n
+
+double=myfunc(2)
+trible=myfunc(3)
+
+print(double(22))
+print(trible(33))'''
+
+
+
---
+++ class and objects/mathfunc.py
@@ -0,0 +1,9 @@
+import math
+print(math.sqrt(2))
+print(math.factorial(5))
+print(math.fabs(-5))
+print(math.pow(2,3))
+print(math.log2(2))
+
+#constant values
+print(math.pi)

ba957995557efbb0a37b76f0fe85321c3b606ec8
crazycelo <celoambrose2116@gmail.com>
2024-06-14 13:17:09 +0530

    add files

---
+++ class and objects/constructor.py
@@ -0,0 +1,13 @@
+class user:
+    def __init__(self,name):
+        self.name=name
+    def printall(self):
+        print("Name:",self.name) 
+
+o=user("celine")
+o.printall()
+print(o.__dict__)
+o1=user("ambrose")
+o1.printall()
+print(o1.__dict__)
+          
